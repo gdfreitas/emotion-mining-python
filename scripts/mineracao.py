825fd65d0a50e98a8c071f4568b2e48a72754809
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2018-08-10 23:01:49 -0300

    criado arquivo de base para mineracao com base de dados com frases e classificacoes

---
+++ scripts/mineracao.py
@@ -0,0 +1,26 @@
+import nltk
+
+base = [
+   ('eu sou admirada por muitos','alegria'),
+   ('me sinto completamente amado','alegria'),
+   ('amar e maravilhoso','alegria'),
+   ('estou me sentindo muito animado novamente','alegria'),
+   ('eu estou muito bem hoje','alegria'),
+   ('que belo dia para dirigir um carro novo','alegria'),
+   ('o dia está muito bonito','alegria'),
+   ('estou contente com o resultado do teste que fiz no dia de ontem','alegria'),
+   ('o amor e lindo','alegria'),
+   ('nossa amizade e amor vai durar para sempre', 'alegria'),
+   ('estou amedrontado', 'medo'),
+   ('ele esta me ameacando a dias', 'medo'),
+   ('isso me deixa apavorada', 'medo'),
+   ('este lugar e apavorante', 'medo'),
+   ('se perdermos outro jogo seremos eliminados e isso me deixa com pavor', 'medo'),
+   ('tome cuidado com o lobisomem', 'medo'),
+   ('se eles descobrirem estamos encrencados', 'medo'),
+   ('estou tremendo de medo', 'medo'),
+   ('eu tenho muito medo dele', 'medo'),
+   ('estou com medo do resultado dos meus testes', 'medo')
+]
+
+print(base)
